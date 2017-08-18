8d4321945b9c79eecccfa8ae81e9c1c32c65a2d8
Juan Li Puma <juanlipuma94@gmail.com>
2017-08-17 22:43:25 -0300

    Use vectors for particle position, add IDs to particles

--- ss/util/particle.py
+++ ss/util/particle.py
@@ -1,22 +1,29 @@
 from math import sqrt
+from uuid import uuid4 as uuid
+from euclid3 import Vector2
 
 
 class Particle:
     """Model class for particles used in simulations"""
 
     def __init__(self, x, y, radius=0):
+        self._id = uuid().int
         self.radius = radius
-        self.moveTo(x, y)
-        # TODO id
-        # TODO vectores de velocidad, posición
+        self.position = Vector2(x, y)
+        self.velocity = Vector2()
+        # TODO usar vectores de velocidad
 
-    def moveTo(self, x, y):
-        self.x = x
-        self.y = y
+    def move_to(self, x, y):
+        self.position = Vector2(x, y)
 
-    def distanceTo(self, other):
-        centerDistance = sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
-        return centerDistance - self.radius - other.radius
+    def distance_to(self, other):
+        # centerDistance = sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
+        center_distance = abs(other.position - self.position)
+        return center_distance - self.radius - other.radius
+
+    @property
+    def id(self):
+        return self._id
 
     def __str__(self) -> str:
-        return "Particle (%g, %g)" % (self.x, self.y)
+        return "Particle (%g, %g)" % (self.position.x, self.position.y)
