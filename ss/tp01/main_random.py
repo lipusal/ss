1c914896f6d75588c277c2367f350e4b3181a056
natalia navas <natinavas@gmail.com>
2017-08-22 21:52:12 -0300

    Recieve arguments by parameter in random main

--- ss/tp01/main_random.py
+++ ss/tp01/main_random.py
@@ -1,16 +1,21 @@
 import random
-
+import argparse
 from ss.cim.cell_index_method import CellIndexMethod
 from ss.cim.particle import Particle
 from ss.util.file_writer import FileWriter
 
-l = 25
-m = 5
-r = 10
+parser = argparse.ArgumentParser(description="Cell Index Method program, runs method on a random set of particles")
+parser.add_argument("amount", help="Amount of particles", default=100, type=int)
+parser.add_argument("boardSize", help="Size of each side of the board", default=250, type=int)
+parser.add_argument("radius", help="Interaction radius for all particles.", default=10.0, type=float)
+args = parser.parse_args()
+
+l = args.boardSize
+r = args.radius
 
 particles = []
 print("Randomly generated particles:")
-for i in range(100):
+for i in range(args.amount):
     particles.append(Particle(random.random() * l, random.random() * l))
     print(particles[-1])
 print()
