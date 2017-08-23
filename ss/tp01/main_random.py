a83c87b2400f43e840e665dc1c4b2123ea55122d
Juan Li Puma <juan.lipuma@restorando.com>
2017-08-23 11:57:40 -0300

    Add switches to random main

--- ss/tp01/main_random.py
+++ ss/tp01/main_random.py
@@ -8,8 +8,15 @@ parser = argparse.ArgumentParser(description="Cell Index Method program, runs me
 parser.add_argument("amount", help="Amount of particles", default=100, type=int)
 parser.add_argument("boardSize", help="Size of each side of the board", default=250, type=int)
 parser.add_argument("radius", help="Interaction radius for all particles.", default=10.0, type=float)
-parser.add_argument("--output", "-o", help="Path of output file. Defaults to './output.txt'", default="./outputRandom.txt")
+parser.add_argument("--output", "-o", help="Path of output file. Defaults to './output.txt'", default="./output.txt")
+parser.add_argument("--periodic", "-p", help="Make the board periodic (particles that go \"out of board\" come in from"
+                                             "the other side", action="store_true", default=False)
+parser.add_argument("--verbose", "-v", help="Print verbose information while running", action="store_true", default=False)
+parser.add_argument("--time", "-t", help="Print elapsed program time", action="store_true", default=False)
 args = parser.parse_args()
+
+if args.time:
+    import ss.util.timer
 
 l = args.boardSize
 r = args.radius
@@ -21,7 +28,7 @@ for i in range(args.amount):
     print(particles[-1])
 print()
 
-data = CellIndexMethod(*particles,interaction_radius=r, is_periodic=True)
+data = CellIndexMethod(*particles,interaction_radius=r, is_periodic=args.periodic)
 
 print('Writing MATLAB output')
 FileWriter.export_positions_matlab(data, 0, args.output)
