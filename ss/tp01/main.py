4cab967b45ae950e55ca67a9f6f176f616aac63a
Juan Li Puma <juanlipuma94@gmail.com>
2017-08-22 22:36:56 -0300

    Add verbose and timing options
    - Only print when --verbose switch is supplied
    - Print program execution time with --time switch

--- ss/tp01/main.py
+++ ss/tp01/main.py
@@ -1,10 +1,10 @@
+import argparse
 import pprint
+from os import path
 
 from ss.cim.cell_index_method import CellIndexMethod
-from ss.util.file_writer import FileWriter
 from ss.util.file_reader import FileReader
-from os import path
-import argparse
+from ss.util.file_writer import FileWriter
 
 # Prepare argument parser
 parser = argparse.ArgumentParser(description="Cell Index Method program, runs method on a given set of particles")
@@ -14,9 +14,18 @@ parser.add_argument("static", nargs="?",
                     default=None)
 parser.add_argument("radius", help="Interaction radius for all particles.", type=float)
 parser.add_argument("--output", "-o", help="Path of output file. Defaults to './output.txt'", default="./output.txt")
-parser.add_argument("--periodic", "-p", help="Make the board periodic (particles that go \"out of board\" come in from the other side", action="store_true", default=False)
+parser.add_argument("--periodic", "-p", help="Make the board periodic (particles that go \"out of board\" come in from"
+                                             "the other side", action="store_true", default=False)
+parser.add_argument("--verbose", "-v", help="Print verbose information while running", action="store_true", default=False)
+parser.add_argument("--time", "-t", help="Print elapsed program time", action="store_true", default=False)
 args = parser.parse_args()
-pprint.pprint(args)
+
+if args.time:
+    import ss.util.timer
+
+if args.verbose:
+    print("Received arguments: ", end='')
+    pprint.pprint(args)
 
 # Normalize paths so they work in any machine
 args.dynamic = path.normpath(args.dynamic)
@@ -29,20 +38,23 @@ particles = imported_data['particles']
 
 data = CellIndexMethod(*particles, interaction_radius=args.radius, is_periodic=args.periodic)
 
-for i in range(len(particles)):
-    print('#%i: %s' % (i + 1, particles[i]))
+if args.verbose:
+    for i in range(len(particles)):
+        print('#%i: %s' % (i + 1, particles[i]))
 
-print('# of particles per cell:')
-# TODO: Move this to CellIndexMethod#__str__
-for row in reversed(range(data.cells_per_row)):
-    print('|', end='')
-    for col in range(data.cells_per_row):
-        print("%i|" % len(data.board[row][col].particles), end='')
-    print()
+        print('# of particles per cell:')
+        # TODO: Move this to CellIndexMethod#__str__
+        for row in reversed(range(data.cells_per_row)):
+            print('|', end='')
+            for col in range(data.cells_per_row):
+                print("%i|" % len(data.board[row][col].particles), end='')
+            print()
 
-print('Distances:')
-pprint.pprint(data.distances)
+        print('Distances:')
+        pprint.pprint(data.distances)
 
-print('Writing MATLAB output')
+        print('Writing MATLAB output')
 FileWriter.export_positions_matlab(data, 0, args.output)
 # FileWriter.export_positions_matlab(data, 23, args.output)
+
+print("Done")
