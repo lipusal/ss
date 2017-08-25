fdefb8e5c2aae389312195a6951fafa4cba733f5
natalia navas <natinavas@gmail.com>
2017-08-25 13:47:49 -0300

    Add implementation of the off lattice automata
    -Calculate the new position and new direction of each particle for a given number of iterations

---
+++ ss/tp01/main_selfpropulsed.py
@@ -0,0 +1,55 @@
+from ss.cim.cell_index_method import CellIndexMethod
+from ss.util.file_reader import FileReader
+from ss.util.file_writer import FileWriter
+
+import argparse
+import math
+
+# Parse args
+parser = argparse.ArgumentParser(description="------")# TODO
+parser.add_argument("radius", help="Interaction radius for all particles.", type=float)
+parser.add_argument("eta", help="Noise that will be added when calculating the variation in angles of each particle", type=float)
+parser.add_argument("-l", help="Board side length. Integer. If not provided, will calculate a minimum bounding box "
+                               "containing all particles", type=int)
+parser.add_argument("-n", help="Amount of particles", type=int, default=100)
+parser.add_argument("--iterations", "-i", help="Amount of iterations", type=int, default=100)
+parser.add_argument("--time", "-t", help="Print elapsed program time", action="store_true", default=False)
+args = parser.parse_args()
+
+if args.time:
+    import ss.util.timer
+
+#TODO randomize
+imported_data = FileReader.import_particles("../../ex/01/Dynamic100.txt", "../../ex/01/Static100.txt")
+particles = imported_data['particles']
+v = 0.3
+delta_t = 1 #TODO siempre usamos 1 no?
+
+# TODO TEST
+
+def avg_angle(neighbors):
+    sin_accum = 0
+    cos_accum = 0
+    len = 0
+    for n in neighbors:
+        len+=1
+        sin_accum += math.sin(n[0].vel_angle())
+        cos_accum += math.cos(n[0].vel_angle())
+    return (sin_accum/len)/(cos_accum/len)
+
+for i in range (1,args.iterations):
+    data = CellIndexMethod(particles, args)
+    for n in data.neighbors:
+        for particleTuple in data.neighbors[n]:
+            particle = particleTuple [0]
+
+            #move position
+            newPositionX = particle.x + particle.velocity[0]*delta_t
+            newPositionY = particle.y + particle.velocity[1]*delta_t
+            particle.move_to(newPositionX, newPositionY)
+
+            #change direction
+            newVelAngle = particle.vel_angle() + avg_angle(data.neighbors[n])
+            particle.velocity = (v, newVelAngle)
+
+print("Done")
