70833fa73bfe38bd05c1f518436b53eaeaa0ba1c
natalia navas <natinavas@gmail.com>
2017-08-25 15:20:50 -0300

    Random particles are generated
    -random direction
    -constant v =0.3

--- ss/tp01/main_selfpropulsed.py
+++ ss/tp01/main_selfpropulsed.py
@@ -1,16 +1,17 @@
 from ss.cim.cell_index_method import CellIndexMethod
-from ss.util.file_reader import FileReader
+from ss.cim.particle import Particle
 from ss.util.file_writer import FileWriter
 
 import argparse
 import math
+import random
 
 # Parse args
 parser = argparse.ArgumentParser(description="------")# TODO
 parser.add_argument("radius", help="Interaction radius for all particles.", type=float)
 parser.add_argument("eta", help="Noise that will be added when calculating the variation in angles of each particle", type=float)
 parser.add_argument("-l", help="Board side length. Integer. If not provided, will calculate a minimum bounding box "
-                               "containing all particles", type=int)
+                               "containing all particles", type=float, default=100.0)
 parser.add_argument("-n", help="Amount of particles", type=int, default=100)
 parser.add_argument("--iterations", "-i", help="Amount of iterations", type=int, default=100)
 parser.add_argument("--time", "-t", help="Print elapsed program time", action="store_true", default=False)
@@ -20,12 +21,22 @@ if args.time:
     import ss.util.timer
 
 #TODO randomize
-imported_data = FileReader.import_particles("../../ex/01/Dynamic100.txt", "../../ex/01/Static100.txt")
-particles = imported_data['particles']
-v = 0.3
+# imported_data = FileReader.import_particles("../../ex/01/Dynamic100.txt", "../../ex/01/Static100.txt")
+# particles = imported_data['particles']
+
+particle_velocity = 0.3
+particles = list()
+for particle_count in range (0, args.iterations):
+    x = random.uniform(0.0, args.l)
+    y = random.uniform(0.0, args.l)
+    o = random.uniform(0.0, 360.0)
+    particles.append(Particle(x, y, 0.0, particle_velocity, o))
+
 delta_t = 1 #TODO siempre usamos 1 no?
 
 # TODO TEST
+
+print(particles)
 
 def avg_angle(neighbors):
     sin_accum = 0
@@ -49,7 +60,9 @@ for i in range (1,args.iterations):
             particle.move_to(newPositionX, newPositionY)
 
             #change direction
+            noise =
             newVelAngle = particle.vel_angle() + avg_angle(data.neighbors[n])
-            particle.velocity = (v, newVelAngle)
+            particle.velocity = (particle_velocity, newVelAngle)
+            print(particle.velocity)
 
 print("Done")
