1c3bb50a1a2caaa2e3eb2935ab187930bd19ad70
Juan Li Puma <juan.lipuma@restorando.com>
2017-08-30 11:08:27 -0300

    Allow running from command line

--- ss/tp02/main_selfpropulsed.py
+++ ss/tp02/main_selfpropulsed.py
@@ -1,3 +1,11 @@
+# Add project to python path so this can be run from the command line, https://stackoverflow.com/a/23891673/2333689
+import sys
+import os
+
+# NOTE: Adjust the number of ".." to get to the project's root directory (i.e. where doc, ex, and ss are, NOT inside ss
+# where cim, util, etc. are)
+sys.path.append(os.path.normpath(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "..")))
+
 import math
 import random
 import colorsys
