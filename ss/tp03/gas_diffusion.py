4c290c37a3ba37186237f0266c4586014ad0755e
Juan Li Puma <juan.lipuma@restorando.com>
2017-09-08 11:34:57 -0300

    Consider particle collisions
    - Calculate collision times between pairs of particles
    - When evolving particles, consider min between wall collision and particle collision
    - Actual collision is NOT yet implemented

--- ss/tp03/gas_diffusion.py
+++ ss/tp03/gas_diffusion.py
@@ -17,37 +17,110 @@ height = 0.09
 width = 0.24
 
 
-def calculate_collision_times(min_collision_times):
+def time_to_wall_collision(particle):
+    """Calculate time for the given particle to collide against any wall. Return infinity if particle will not collide
+    with a wall (e.g. if Vx = Vy = 0)."""
+    # TODO: Return which wall the collision will be with?
     # TODO ver donde estan las paredes, esta muy hardcodeado el width y el 0! y donde hay ranuras y otras particulas
 
-    for particle in particles:
+    # Calculate minimum collision time in x axis
+    if particle.velocity.x > 0:
+        collision_time_x = (width - particle.radius - particle.position.x) / particle.velocity.x
+    elif particle.velocity.x < 0:
+        collision_time_x = (0 + particle.radius - particle.position.x) / particle.velocity.x
+    else:
+        collision_time_x = math.inf
 
-        # Calculate minimum collision time in x axis
-        if particle.velocity.x > 0:
-            collision_time_x = (width - particle.radius - particle.position.x) / particle.velocity.x
-        elif particle.velocity.x < 0:
-            collision_time_x = (0 + particle.radius - particle.position.x) / particle.velocity.x
+    # Calculate minimum collision time in y axis
+    if particle.velocity.y > 0:
+        collision_time_y = (height - particle.radius - particle.position.y) / particle.velocity.y
+    elif particle.velocity.y < 0:
+        collision_time_y = (0 + particle.radius - particle.position.y) / particle.velocity.y
+    else:
+        collision_time_y = math.inf
+
+    # Set minimum time for collision on either axis
+    result = min(collision_time_x, collision_time_y)
+    if result < 0:
+        raise ValueError('Collision time should never be a negative value')
+
+    return result
+
+
+def time_to_particle_collision(particle1, particle2):
+    """Calculate time for the two given particles to collide. Return infinity if they will not collide."""
+
+    delta_r = particle2.position - particle1.position
+    delta_v = particle2.velocity - particle1.velocity
+    v_r = delta_v.dot(delta_r)
+
+    if v_r >= 0:
+        return math.inf
+
+    o = particle1.radius + particle2.radius
+    v2 = delta_v.dot(delta_v)
+    d = (v_r ** 2) - v2 * (delta_r.dot(delta_r) - (o ** 2))
+    if d < 0:
+        return math.inf
+
+    result = -(v_r + math.sqrt(d)) / v2
+    if result < 0:
+        # TODO: Make sure particles are not generate with overlap
+        print("WARNING: %s and %s overlap, giving a negative collision time. Ignoring and returning infinity, "
+              "make sure particles are not generated with overlap!" % (particle1, particle2))
+        return math.inf
+
+    return -(v_r + math.sqrt(d)) / v2
+
+
+def min_collision_times(particles):
+    """Returns a dictionary of the form (particle_id, (min_collision_time, collision_target)). That is, for each
+    particle, the minimum time for it to collide against a wall or a particle, and against what it will collide at that
+    time."""
+
+    result = dict()
+    for i in range(len(particles) - 1):
+        me = particles[i]
+        wall_time = time_to_wall_collision(me)
+        particle_time = math.inf
+        other_particle = None
+
+        # Get the other particle that this particle will first collide with. Iterate from the next particle to the end;
+        # there's no need to loop over all particles again, since time_to_collision(x, y) == time_to_collision(y, x)
+        for j in range(i + 1, len(particles)):
+            t = time_to_particle_collision(me, particles[j])
+            if t < particle_time:
+                particle_time = t
+                other_particle = particles[j]
+
+        if wall_time <= particle_time or math.inf:
+            # Contemplates the (very rare) case in which a particle will never collide against anything, will insert
+            # (math.inf, None). Ignores cases in which a particle collides against a wall and another particle at the
+            # same time; takes only the wall collision
+            # TODO mark against which wall it will collide?
+            result[me.id] = (wall_time, None)
         else:
-            collision_time_x = math.inf
+            result[me.id] = (particle_time, other_particle)
+            result[other_particle.id] = (particle_time, me)
 
-        # Calculate minimum collision time in y axis
-        if particle.velocity.y > 0:
-            collision_time_y = (height - particle.radius - particle.position.y) / particle.velocity.y
-        elif particle.velocity.y < 0:
-            collision_time_y = (0 + particle.radius - particle.position.y) / particle.velocity.y
-        else:
-            collision_time_y = math.inf
-
-        # Set minimum time for collision on either axis
-        # TODO: Mark that particle will collide with wall, i.e. None?
-        min_collision_times[particle.id] = min(collision_time_x, collision_time_y)
-
-        # Raise an exception when the collision time is negative
-        if min_collision_times[particle.id] < 0:
-            raise ValueError('Collision time should never be a negative value')
+    return result
 
 
-def evolve_particles(time):
+def min_collision_time(data):
+    """Get the minimum collision time of all times in the specified data, where data has the form of what is returned
+    by #min_collision_times. Return (min_time, target)"""
+    result = None
+    min_time = math.inf
+
+    for particle_id, (time, target) in data.items():
+        if time < min_time:
+            min_time = time
+            result = (min_time, target)
+
+    return result
+
+
+def evolve_particles(particles, time):
     for particle in particles:
 
         # Move particle
@@ -113,14 +186,18 @@ for particle_count in range(arguments.n):
     # TODO: Consider making Ovito color particles so we don't have to recalculate colors all the time
     colors.append(radians_to_rgb(o))
 
-# while fp_left > 0.5:
 t = 0
+collision_times = min_collision_times(particles)
+
+# while fp_left > 0.5:
 for i in range(500):
-    # TODO: cambiar solo los que iban a chocar con algo que choco no fuerza bruta a lo loco
-    min_collision_times = dict()
-    calculate_collision_times(min_collision_times)
-    min_time = min(min_collision_times.values())
-    evolve_particles(min_time)
+    min_time, target = min_collision_time(collision_times)
+    evolve_particles(particles, min_time)
+
+    # Subtract min_time from all collision times; colliding particle(s) will have their collision time set to 0
+    for particle_id, (time, target) in collision_times.items():
+        collision_times[particle_id] = (time - min_time, target)
+
     t += min_time
     recalculate_fp()
     FileWriter.export_positions_ovito(particles, t=t, colors=colors, output=arguments.output,
