64f72dbfa029e009740980d6f3de0f130258bdd8
Juan Li Puma <juan.lipuma@restorando.com>
2017-09-29 16:43:49 -0300

    Add gas exercise constants

---
+++ ss/tp04/gas/main.py
@@ -0,0 +1,20 @@
+from ss.cim.cell_index_method import CellIndexMethod
+import ss.util.args as arg_base
+
+arg_base.parser.description = "Gas Diffusion simulation Program. Simulates how a number of given gas particles " \
+                              "diffuse from one compartment to another through a slit. Unlike TP03, particles in this" \
+                              "simulation have interaction forces as modeled by the Lennard-Jones potential model"
+args = arg_base.parse_args()
+
+# Constants
+R_M = 1         # Rm, distance of minimum potential. If particles are closer than this, they are
+                # repelled [dimensionless]
+EPSILON = 2     # ε, depth of potential well [J]
+M = 0.1         # Particle mass [dimensionless]
+V0 = 10         # Initial particle speed [dimensionless]
+R = 5           # Maximum interaction distance [dimensionless]
+WIDTH = 400     # Area width. Each compartment has width WIDTH/2 [dimensionless]
+HEIGHT = 200    # Area height [dimensionless]
+SLIT_SIZE = 10  # [dimensionless]
+NUM_PARTICLES = 1000
+fp = 1          # particles on left compartment / total particles (ie. all particles start on the left compartment)
