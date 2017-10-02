4f1628c967684ae6820dc4fd09b05faf7f884b01
Juan Li Puma <juan.lipuma@restorando.com>
2017-10-02 19:27:03 -0300

    Revise Gear-Predictor
    - Write used formulas, add debug prints
    - Add missing parentheses
    - Simplify some formulas

--- ss/tp04/solutions/gear_predictor.py
+++ ss/tp04/solutions/gear_predictor.py
@@ -26,31 +26,40 @@ def run(particle, delta_t):
 
 
 def predict(particle, delta_t):
-    """Predict the 5 derivatives of the position for the given delta T, using the original 5 derivatives."""
+    """Predict the 5 derivatives of the position for the given delta T, using the original 5 derivatives.
+    rP[5] (t + Δt) = r[5](t)
+    rP[4] (t + Δt) = r[4](t) + r[5](t) * Δt^1 / 1!
+    rP[3] (t + Δt) = r[3](t) + r[4](t) * Δt^2 / 2!
+    ...
+    """
 
     result = [0] * (DEGREE + 1)
     rs = rs_oscillator(particle)
     for i in range(DEGREE, -1, -1):
+        # debug = "rP[%i](t+Δt) = r[%i](t)" % (i, i)
         result[i] = rs[i]
         k = 1
         for j in range(i+1, DEGREE+1):
             result[i] += rs[j]*(delta_t**k)/math.factorial(k)
+            # debug += " + r[%i]*Δt^%i/%i!" % (j, k, k)
             k += 1
+
+        # print(debug)
 
     return result
 
 
 def evaluate(particle, delta_t, r_ps):
-    # Evaluate R2 in RP0, RP1 (predicted position, predicted velocity) to get future acceleration
-    delta_a = future_a_oscillator(particle, r_ps) - r_ps[2]
+    # Evaluate r[2] in rP[0], rP[1] (predicted position, predicted velocity) to get future acceleration
+    delta_a = r2_oscillator(particle, r_ps) - r_ps[2]
 
     return delta_a * (delta_t**2) / math.factorial(2)   # Delta R2
 
 
 def correct(r_ps, alphas, delta_r2, delta_t):
-    result = [0] * (DEGREE + 1)
+    result = list()
     for i in range(DEGREE + 1):
-        result[i] = r_ps[i] + alphas[i] * delta_r2 * (math.factorial(i) / (delta_t**i))
+        result.append(r_ps[i] + (alphas[i] * delta_r2 * math.factorial(i) / (delta_t**i)))
 
     return result
 
@@ -60,12 +69,15 @@ def rs_oscillator(particle):
     oscillator."""
 
     result = [particle.position, particle.velocity]     # R0, R1
-    for i in range(2, DEGREE+1):   # R2, R3, R4, R5
-        result.append(((result[i-2] * -K) - result[i-1] * lamb) / particle.mass)
+    # For oscillator, for every n >= 2, r[n] = (K * r[n-2] + lambda * r[n-1]) / -mass
+    for n in range(2, DEGREE+1):   # R2, R3, R4, R5
+        result.append((K*result[n-2] + lamb*result[n-1]) / -particle.mass)
     return result
 
 
-def future_a_oscillator(particle, r_ps):
+def r2_oscillator(particle, r_ps):
     """Get future acceleration of an oscillator with the given current state and predicted future derivatives. This is
     specific to the oscillator."""
-    return ((r_ps[0] * -K) - r_ps[1] * lamb) / particle.mass
+
+    # r[2] = (K*r[0] + lambda*r[1]) / -mass (see `rs_oscillator`)
+    return (K*r_ps[0] + lamb*r_ps[1]) / -particle.mass
