26b6af0242b89ed160dd5e61f4c71b53df136004
Juan Li Puma <juan.lipuma@restorando.com>
2017-10-18 23:42:04 -0300

    Add sliding window code script

--- ss/tp05/flow_sliding_window.py
+++ ss/tp05/flow_sliding_window.py
@@ -9,14 +9,27 @@ def sliding_window_flow(window_size, input="flow_n.txt", output="sliding_window.
 
     file = open(output, mode="w")
     width = window_size // 2
-    i = width
-    while i + width <= len(ns):
-        end, start = i + width, i - width
+    i = 0
+    while i + width - 1 < len(ns):
+        start, end = i - width, i + width - 1
         flow = (ns[end] - ns[start]) / (ts[end] - ts[start])
         file.write("%i,%g\n" % (i, flow))
+        i += 1
+    file.close()
 
 
 def append_event(output, num_fallen_particles, t, mode="a"):
     file = open(output, mode)
     file.write('%i,%g\n' % (num_fallen_particles, t))
     file.close()
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("window_size", help="Window size to use.", type=int)
+    parser.add_argument("--input", "-i", help="Input file. Default is 'flow_n.txt'", type=str, default='flow_n.txt')
+    parser.add_argument("--output", "-o", help="Output file. Default is 'sliding_window.txt'", type=str, default="sliding_window.txt")
+    args = parser.parse_args()
+
+    sliding_window_flow(args.window_size, args.input, args.output)
