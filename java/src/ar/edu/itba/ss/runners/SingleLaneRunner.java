0f670fd7bea282db5fb0ddeca9411fc9b23942ec
natalia navas <natinavas@gmail.com>
2018-12-09 18:41:38 -0300

    Add main method to Single Lane Runner

--- java/src/ar/edu/itba/ss/runners/SingleLaneRunner.java
+++ java/src/ar/edu/itba/ss/runners/SingleLaneRunner.java
@@ -1,37 +1,44 @@
 package ar.edu.itba.ss.runners;
 
+import ar.edu.itba.ss.files.OvitoWriter;
+import ar.edu.itba.ss.models.KSSS;
 import ar.edu.itba.ss.particles.Car;
+import ar.edu.itba.ss.particles.Particle;
 
 import java.awt.geom.Point2D;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static ar.edu.itba.ss.util.CarUtils.randomCarsOnY;
+import static ar.edu.itba.ss.util.OvitoUtils.withPlaceholders;
 
 public class SingleLaneRunner {
 
-    /**
-     * Generate a list of cars with V0 = 0 in random positions along the road with given length.
-     *
-     * @param carCount    Number of cars to generate
-     * @param roadLength  Road length
-     * @param minDistance Min distance between cars
-     * @return The resulting cars
-     */
-    private List<Car> randomCars(int carCount, int roadLength, int minDistance) {
-        // TODO: Consider minDistance? Maybe we can't have cars directly next to each other. That might require to
-        // TODO: start this method from scratch
+    public static void main(String[] args) throws IOException {
+        final int ROAD_LENGTH = 200,
+                MAX_SPEED = 20;
+        final double P = 0;
+        final int car_radius = 5;
+        OvitoWriter<Particle> ovitoWriter = new OvitoWriter<>(Paths.get("out.txt"));
 
-        // Build range from 0 to roadLength (exclusive)
-        List<Integer> allPositions = IntStream
-                .range(0, roadLength)
-                .boxed()
-                .collect(Collectors.toList());
-        // Shuffle it
-        Collections.shuffle(allPositions);
-        // Return the first carCount positions, mapped to cars
-        return allPositions.stream()
-                .limit(carCount)
-                .map(position -> new Car(new Point2D.Double(position, 0)))
-                .collect(Collectors.toList());
+        List<Car> placeholders = new ArrayList<>(2);
+        placeholders.add(new Car(new Point2D.Double(0, 0), 0.1).fake());
+        placeholders.add(new Car(new Point2D.Double(ROAD_LENGTH, 0), 0.1).fake());
+
+        List<Car> cars = randomCarsOnY(10, ROAD_LENGTH, 0, 0);
+        KSSS model = new KSSS(ROAD_LENGTH, MAX_SPEED, cars);
+//        NaSchModel model = new NaSchModel(ROAD_LENGTH, MAX_SPEED, P, cars);
+        int t = 0;
+        while (t < 250) { // TODO: parametrizar tiempo de simulación
+            List<Particle> allCars = withPlaceholders(placeholders, cars);
+            ovitoWriter.exportPositions(allCars, t);
+            cars = model.evolve();
+            t++;
+        }
+        ovitoWriter.close();
     }
+
+
 }
