b62ddea6d0856f946e8cb87578b39ec390525ab6
natalia navas <natinavas@gmail.com>
2019-02-03 20:00:03 -0300

    Fixes in green light runner

--- java/src/ar/edu/itba/ss/runners/singleLane/LiPumaNavasGreenLights.java
+++ java/src/ar/edu/itba/ss/runners/singleLane/LiPumaNavasGreenLights.java
@@ -16,41 +16,72 @@ public class LiPumaNavasGreenLights extends Runner{
 
     @SuppressWarnings("Duplicates")
     public static void main(String[] args) throws IOException {
-        final int ROAD_LENGTH = 1000,
-                MAX_SPEED = 20;
+        final int ROAD_LENGTH = 500,
+                MAX_SPEED = 3;
         final int car_radius = 1;
-        OvitoWriter<Particle> ovitoWriter = new OvitoWriter<>(Paths.get("out.txt"));
+        boolean errored = false;
+        do {
+            OvitoWriter<Particle> ovitoWriter = null;
+            try {
 
-        List<Car> placeholders = new ArrayList<>(2);
+                int greenDurationOne = 20;
+                int redDurationOne = 10;
+                int phaseOne = 0;
+
+                int greenDurationTwo = 20;
+                int redDurationTwo = 10;
+                int phaseTwo = 0;
+
+                int greenDurationThree = 20;
+                int redDurationThree = 10;
+                int phaseThree = 0;
+
+
+                ovitoWriter = new OvitoWriter<>(Paths.get("out.txt"));
+
+                List<Car> placeholders = new ArrayList<>(2);
 
         /* *************************************************************************************************************
          *                                          HORIZONTAL MODEL
          * ************************************************************************************************************/
-        placeholders.add(new Car(new Point2D.Double(0, ROAD_LENGTH/2.0 + 20), 0.1).fake());
-        placeholders.add(new Car(new Point2D.Double(ROAD_LENGTH, ROAD_LENGTH/2.0 - 20), 0.1).fake());
+                placeholders.add(new Car(new Point2D.Double(0, ROAD_LENGTH / 2.0 + 20), 0.1).fake());
+                placeholders.add(new Car(new Point2D.Double(ROAD_LENGTH, ROAD_LENGTH / 2.0 - 20), 0.1).fake());
+                placeholders.add(new Car(new Point2D.Double(ROAD_LENGTH, 0), 0.1).fake());
+                placeholders.add(new Car(new Point2D.Double(0, 0), 0.1).fake());
 
-        List<Car> carsH = new ArrayList<>();
-        carsH.add(new Car(new Point2D.Double(0, ROAD_LENGTH/2.0), new Point2D.Double(2, 0), car_radius));
-        carsH.add(new Car(new Point2D.Double(10, ROAD_LENGTH/2.0), new Point2D.Double(1, 0), car_radius));
-//        cars.add(new Car(new Point2D.Double(7, 0), new Point2D.Double(2, 0)));
-        carsH.add(new Car(new Point2D.Double(20, ROAD_LENGTH/2.0), car_radius));
+                List<Car> carsH = new ArrayList<>();
+                carsH.add(new Car(new Point2D.Double(0, 0), new Point2D.Double(2, 0), car_radius));
+                carsH.add(new Car(new Point2D.Double(10, 0), new Point2D.Double(1, 0), car_radius));
+                carsH.add(new Car(new Point2D.Double(20, 0), car_radius));
+                carsH.add(new Car(new Point2D.Double(30, 0), car_radius));
+                carsH.add(new Car(new Point2D.Double(40, 0), car_radius));
+                carsH.add(new Car(new Point2D.Double(50, 0), car_radius));
 
-        List<TrafficLight> trafficLightsH = new ArrayList<>();
-        trafficLightsH.add(new TrafficLight(new Point2D.Double(ROAD_LENGTH/2.0 - 5, ROAD_LENGTH/2.0), 25, /*TODO calculate this*/10, 50));
-        trafficLightsH.get(0).setDrawPosition(new Point2D.Double(ROAD_LENGTH/2.0 - 5, ROAD_LENGTH/2.0 - 10));
-        ar.edu.itba.ss.models.LiPumaNavas modelH = new ar.edu.itba.ss.models.LiPumaNavas(ROAD_LENGTH, MAX_SPEED, 3, true, carsH, trafficLightsH);
+                List<TrafficLight> trafficLightsH = new ArrayList<>();
+                trafficLightsH.add(new TrafficLight(new Point2D.Double(ROAD_LENGTH / 4.0, 0), redDurationOne, greenDurationOne, phaseOne));
+                trafficLightsH.add(new TrafficLight(new Point2D.Double((2 * ROAD_LENGTH) / 4.0, 0), redDurationTwo, greenDurationTwo,phaseTwo));
+                trafficLightsH.add(new TrafficLight(new Point2D.Double((3 * ROAD_LENGTH) / 4.0, 0), redDurationThree, greenDurationThree, phaseThree));
+                ar.edu.itba.ss.models.LiPumaNavas modelH = new ar.edu.itba.ss.models.LiPumaNavas(ROAD_LENGTH, MAX_SPEED, 3, true, carsH, trafficLightsH);
 
-        int t = 0;
-        while (t < 500) { // TODO: parametrizar tiempo de simulación
-            List<Particle> allCars = withPlaceholders(placeholders, carsH);
-            allCars.addAll(trafficLightsH);
-            ovitoWriter.exportPositions(allCars, t);
-            carsH = modelH.evolve();
-            t++;
-        }
-        ovitoWriter.close();
+                int t = 0;
+                while (t < 500) { // TODO: parametrizar tiempo de simulación
+                    List<Particle> allCars = withPlaceholders(placeholders, carsH);
+                    allCars.addAll(trafficLightsH);
+                    ovitoWriter.exportPositions(allCars, t);
+                    carsH = modelH.evolve();
+                    t++;
+                }
+                ovitoWriter.close();
+            } catch (IllegalStateException | IllegalArgumentException e) {
+                errored = true;
+                System.out.format("Error in run. Retrying\n");
+
+            }
+            finally {
+                if (ovitoWriter != null) {
+                    ovitoWriter.close();
+                }
+            }
+        }while (errored);
     }
-
-
-
 }
