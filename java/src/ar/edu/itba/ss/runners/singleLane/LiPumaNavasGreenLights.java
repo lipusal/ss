41b4579271b1aebdac60caa8b885fa66f6fc93db
Juan Li Puma <juanlipuma94@gmail.com>
2019-02-03 23:37:49 -0500

    First attempt
    - Extract some constants
    - Make a first attempt to make onda verde
      - First car never stops, perfect
      - Following cars always get stuck in traffic lights

--- java/src/ar/edu/itba/ss/runners/singleLane/LiPumaNavasGreenLights.java
+++ java/src/ar/edu/itba/ss/runners/singleLane/LiPumaNavasGreenLights.java
@@ -17,12 +17,15 @@ public class LiPumaNavasGreenLights extends Runner {
     @SuppressWarnings("Duplicates")
     public static void main(String[] args) throws IOException {
         final int ROAD_LENGTH = 500,    // 500 * 7.5m = 3.75km
-                MAX_SPEED = 3;          // 3 * 7.5 * 3.6 = 81km/h
+                MAX_SPEED = 3,          // 3 * 7.5 * 3.6 = 81km/h
+                SECURITY_GAP = 1;
         final double car_radius = 0.5;
         boolean errored = false;
         do {
             OvitoWriter<Particle> ovitoWriter = null;
             try {
+                double deltaT = (ROAD_LENGTH/4.0) / MAX_SPEED; // Distance between lights / VMax = t min for cars to get from one light to the next
+                // TODO try making green last as long as needed for all cars to pass. But all traffic lights should have the same durations
                 // First traffic light
                 int greenDurationOne = 20;
                 int redDurationOne = 10;
@@ -30,11 +33,11 @@ public class LiPumaNavasGreenLights extends Runner {
                 // Second traffic light
                 int greenDurationTwo = 20;
                 int redDurationTwo = 10;
-                int phaseTwo = 0;
+                int phaseTwo = (int) deltaT % greenDurationOne;
                 // Third traffic light
                 int greenDurationThree = 20;
                 int redDurationThree = 10;
-                int phaseThree = 0;
+                int phaseThree = (int) (2 * deltaT) % greenDurationOne;
 
                 ovitoWriter = new OvitoWriter<>(Paths.get("out.txt"));
                 List<Car> placeholders = new ArrayList<>(2);
@@ -56,7 +59,7 @@ public class LiPumaNavasGreenLights extends Runner {
                 trafficLightsH.add(new TrafficLight(new Point2D.Double(ROAD_LENGTH / 4.0, 0), redDurationOne, greenDurationOne, phaseOne));
                 trafficLightsH.add(new TrafficLight(new Point2D.Double((2 * ROAD_LENGTH) / 4.0, 0), redDurationTwo, greenDurationTwo, phaseTwo));
                 trafficLightsH.add(new TrafficLight(new Point2D.Double((3 * ROAD_LENGTH) / 4.0, 0), redDurationThree, greenDurationThree, phaseThree));
-                ar.edu.itba.ss.models.LiPumaNavas modelH = new ar.edu.itba.ss.models.LiPumaNavas(ROAD_LENGTH, MAX_SPEED, 3, true, carsH, trafficLightsH);
+                ar.edu.itba.ss.models.LiPumaNavas modelH = new ar.edu.itba.ss.models.LiPumaNavas(ROAD_LENGTH, MAX_SPEED, SECURITY_GAP, true, carsH, trafficLightsH);
 
                 int t = 0;
                 while (t < 500) { // TODO: parametrizar tiempo de simulación
