d0cf62fc37312cd510473383d91ae77e2a61feb4
Juan Li Puma <juanlipuma94@gmail.com>
2019-02-07 22:21:27 -0500

    Try to catch more edge cases for traffic light interaction
    - When stopped due to traffic light
    - When effective gap is 0
    - When about to run into security gap

--- java/src/ar/edu/itba/ss/models/LiPumaNavas.java
+++ java/src/ar/edu/itba/ss/models/LiPumaNavas.java
@@ -196,10 +196,12 @@ public class LiPumaNavas extends SingleLaneModel {
                 th = wrapAroundDistance(car, nextTrafficLight) / v,
                 ts = Math.min(v, H * 3),                                            // Bigger interaction horizon for traffic lights since they're visible from farther away and cars approach them with a much bigger delta T than with another car
                 remainingTime = nextTrafficLight.getRemainingTime(simTime);
-        return trafficLightDistance < wrapAroundDistance(car, nextCar)              // Traffic light is closer than next car
-                && (!nextTrafficLight.isGreen() || remainingTime <= th)             // Traffic light is not green OR will stop being green before reaching it
-                &&  (th < ts                                                        // Traffic light is within interaction horizon (always false when car is stopped), OR
-                    || (v == 0 && effectiveGap(car, nextTrafficLight) == 0))        // Car is stopped because of the traffic light
+        return trafficLightDistance < wrapAroundDistance(car, nextCar)                      // Traffic light is closer than next car
+                && (!nextTrafficLight.isGreen() || remainingTime <= th)                     // Traffic light is not green OR will stop being green before reaching it
+                &&  (th < ts                                                                // Traffic light is within interaction horizon (always false when car is stopped), OR
+                    || v == 0                                                               // Car is stopped because of the traffic light
+                    || effectiveGap(car, nextTrafficLight) == 0
+                    || getVelocityComponent(car) >= effectiveGap(car, nextTrafficLight))    // Car will run into security gap at this rate
                 && requiredDeceleration(car, nextTrafficLight) >= maxDeceleration;  // Required deceleration is acceptable (>=, not <= because we're dealing with negative numbers)
     }
 
