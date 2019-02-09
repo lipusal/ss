b18c0b39d2c3069fc8b52c6733b848e5154ba623
natalia navas <natinavas@gmail.com>
2019-02-09 12:20:19 -0300

    Add generate car list method with even distribution in Runner abstract class

--- java/src/ar/edu/itba/ss/runners/Runner.java
+++ java/src/ar/edu/itba/ss/runners/Runner.java
@@ -4,6 +4,7 @@ import ar.edu.itba.ss.particles.Car;
 import ar.edu.itba.ss.particles.Particle;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,5 +21,21 @@ public abstract  class Runner {
         Color[] c = new Color[] { Color.RED, Color.BLUE, Color.CYAN };
         int max = c.length;
         return carsWithPlaceholders.stream().map(car -> car.isFake() ? Color.GREEN : c[car.getId() % max]).collect(Collectors.toList());
+    }
+
+    /**
+     * Generates a list of cars that are evenly distributed with the same initial velocity and car radius
+     * @param amountOfCars that will be generated
+     * @param roadLength to distribute the cars in a uniform way
+     * @param carRadius for all the cars that are generated
+     * @param initialVelocity for all the cars that are generated
+     * @return list of generated cars
+     */
+    protected static List<Car> generateCars(int amountOfCars, int roadLength, int carRadius, int initialVelocity){
+        List<Car> cars = new ArrayList<>();
+        for (int i=0; i<roadLength; i+=(roadLength/amountOfCars)){
+            cars.add(new Car(new Point2D.Double(i, 0), new Point2D.Double(initialVelocity, 0), carRadius));
+        }
+        return cars;
     }
 }
