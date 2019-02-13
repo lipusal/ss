da69edd61bebe8fa9eb83e7c292ec0e2cf694f06
Juan Li Puma <juanlipuma94@gmail.com>
2019-02-12 20:43:23 -0500

    Change params for NaSch with immovable

--- java/src/ar/edu/itba/ss/runners/singleLane/NaSchWithImmovable.java
+++ java/src/ar/edu/itba/ss/runners/singleLane/NaSchWithImmovable.java
@@ -17,10 +17,10 @@ public class NaSchWithImmovable extends Runner{
 
     @SuppressWarnings("Duplicates")
     public static void main(String[] args) throws IOException {
-        final int ROAD_LENGTH = 1000,
+        final int ROAD_LENGTH = 300,
                 MAX_SPEED = 20;
         final double P = 0.1;
-        final int car_radius = 1;
+        final double CAR_RADIUS = 0.5;
         OvitoWriter<Particle> ovitoWriter = new OvitoWriter<>(Paths.get("out.txt"));
 
         List<Car> placeholders = new ArrayList<>(2);
@@ -32,15 +32,15 @@ public class NaSchWithImmovable extends Runner{
         placeholders.add(new Car(new Point2D.Double(ROAD_LENGTH, ROAD_LENGTH/2.0 - 20), 0.1).fake());
 
         List<Car> carsH = new ArrayList<>();
-        carsH.add(new Car(new Point2D.Double(0, ROAD_LENGTH/2.0), new Point2D.Double(2, 0), car_radius));
-        carsH.add(new Car(new Point2D.Double(10, ROAD_LENGTH/2.0), new Point2D.Double(1, 0), car_radius));
+        carsH.add(new Car(new Point2D.Double(0, ROAD_LENGTH/2.0), new Point2D.Double(2, 0), CAR_RADIUS));
+        carsH.add(new Car(new Point2D.Double(10, ROAD_LENGTH/2.0), new Point2D.Double(1, 0), CAR_RADIUS));
 //        cars.add(new Car(new Point2D.Double(7, 0), new Point2D.Double(2, 0)));
-        carsH.add(new Car(new Point2D.Double(20, ROAD_LENGTH/2.0), car_radius));
-        carsH.add(new ImmovableCar(new Point2D.Double(ROAD_LENGTH/2.0, ROAD_LENGTH/2.0)));
+        carsH.add(new Car(new Point2D.Double(20, ROAD_LENGTH/2.0), CAR_RADIUS));
+        carsH.add(new ImmovableCar(new Point2D.Double(ROAD_LENGTH - 10, ROAD_LENGTH/2.0)));
 
         NaSch modelH = new NaSch(ROAD_LENGTH, MAX_SPEED, P, carsH);
         int t = 0;
-        while (t < 500) { // TODO: parametrizar tiempo de simulación
+        while (t < 100) {
             List<Particle> allCars = withPlaceholders(placeholders, carsH);
             ovitoWriter.exportPositions(allCars, t);
             carsH = modelH.evolve();
