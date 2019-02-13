c3788cfcc494c5c8b56e79c3099caeafdaed0fa3
Juan Li Puma <juanlipuma94@gmail.com>
2019-02-12 21:18:38 -0500

    Change params for KSSS with immovable

--- java/src/ar/edu/itba/ss/runners/singleLane/KsssWithImmovable.java
+++ java/src/ar/edu/itba/ss/runners/singleLane/KsssWithImmovable.java
@@ -17,10 +17,9 @@ public class KsssWithImmovable extends Runner{
 
     @SuppressWarnings("Duplicates")
     public static void main(String[] args) throws IOException {
-        final int ROAD_LENGTH = 1000,
+        final int ROAD_LENGTH = 300,
                 MAX_SPEED = 20;
-        final double P = 0.1;
-        final int car_radius = 1;
+        final double car_radius = 0.5;
         OvitoWriter<Particle> ovitoWriter = new OvitoWriter<>(Paths.get("out.txt"));
 
         List<Car> placeholders = new ArrayList<>(2);
@@ -32,11 +31,11 @@ public class KsssWithImmovable extends Runner{
         carsH.add(new Car(new Point2D.Double(10, ROAD_LENGTH/2.0), new Point2D.Double(1, 0), car_radius));
 //        cars.add(new Car(new Point2D.Double(7, 0), new Point2D.Double(2, 0)));
         carsH.add(new Car(new Point2D.Double(20, ROAD_LENGTH/2.0), car_radius));
-        carsH.add(new ImmovableCar(new Point2D.Double(ROAD_LENGTH/2.0, ROAD_LENGTH/2.0)));
+        carsH.add(new ImmovableCar(new Point2D.Double(ROAD_LENGTH - 10, ROAD_LENGTH/2.0)));
 
         KSSS modelH = new KSSS(ROAD_LENGTH, MAX_SPEED, 1, true, 0.5, 0.94, 0.1, carsH);
         int t = 0;
-        while (t < 500) { // TODO: parametrizar tiempo de simulación
+        while (t < 100) {
             List<Particle> allCars = withPlaceholders(placeholders, carsH);
             ovitoWriter.exportPositions(allCars, t);
             carsH = modelH.evolve();
